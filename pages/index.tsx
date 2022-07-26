5f797404696a6efa6e12e63c4e7c5ed9e7bf767c
notraiyan <raiyanahmed@iut-dhaka.edu>
2022-07-26 19:00:48 +0600

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
           Welcome to <a href="#">Faizar!</a>
         </h1>
 
-        <Image src="/faizar.jpg" alt="faizar" width={200} height={400} />
+        {/* <Image src="/faizar.jpg" alt="faizar" width={200} height={400} /> */}
       </main>
 
       <footer className={styles.footer}>
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
           </span>
         </a>
       </footer>
