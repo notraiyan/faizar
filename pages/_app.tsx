b36e3855df27b10af83ff78cbc5179dc3d3778bc
notraiyan <raiyanahmed@iut-dhaka.edu>
2022-08-26 01:31:54 +0600

    new way apply

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,89 +20,101 @@ const App = ({ Component, pageProps }: AppProps) => {
 
     return (
       <>
-          <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-              background: {
-                color: {
-                  value: "transparent",
-                },
-              },
-              fpsLimit: 120,
-              interactivity: {
-                events: {
-                  onClick: {
-                    enable: true,
-                      mode: "push",
-                    },
-                    onHover: {
-                      enable: true,
-                      mode: "repulse",
-                    },
-                    resize: true,
-                },
-                modes: {
-                  push: {
-                    quantity: 4,
-                  },
-                  repulse: {
-                    distance: 200,
-                    duration: 0.4,
-                  },
-                },
-              },
-              particles: {
-                color: {
-                  value: "#0d47a1",
-                },
-                links: {
-                  color: "#0d47a1",
-                  distance: 150,
-                  enable: true,
-                  opacity: 0.5,
-                  width: 1,
-                },
-                collisions: {
-                  enable: true,
-                },
-                move: {
-                  enable: true,
-                  outModes: {
-                    default: "bounce",
-                  },
-                  random: false,
-                  speed: 1,
-                  straight: false,
-                },
-                number: {
-                  density: {
-                    enable: true,
-                    area: 800,
-                  },
-                  value: 80,
-                },
-                opacity: {
-                  value: 0.5,
-                },
-                shape: {
-                  type: "circle",
-                },
-                size: {
-                  value: { min: 1, max: 5 },
-                },
-              },
-              detectRetina: true,
-            }}
-            style={{ position: "absolute",
-            width: "100%",
-            zIndex: -1,
-            top: 0,
-            left: 0,
-            opacity: 0.7, }}
-          />
         <Component {...pageProps} />
+        {/* <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "transparent",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "bubble"
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 150,
+                  duration: 0.4,
+                },
+                bubble: {
+                  distance: 200,
+                  size: 20,
+                  duration: 2,
+                  opacity: 8,
+                  speed: 3
+                }
+              },
+            },
+            particles: {
+              color: {
+                value: ["#1abc9c","#3498db","#9b59b6","#f1c40f","#e74c3c"]
+              },
+              links: {
+                color: "#fff",
+                distance: 100,
+                enable: true,
+                opacity: 0.5,
+                width: 5,
+                shadow: {
+                    enable: true,
+                    color: "#333333",
+                    blur: 5
+                }
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: true,
+                speed: 0.6,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 70,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "triangle",
+              },
+              size: {
+                value: 40,
+                random: true
+              },
+            },
+            detectRetina: true,
+          }}
+          style={{ position: "absolute",
+          width: "100%",
+          zIndex: -1,
+          top: 0,
+          left: 0,
+          opacity: 1, }}
+        /> */}
       </>
     );
 };
