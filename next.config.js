8cb28e4904b790e436a390aa30206a6b350db44e
notraiyan <raiyanahmed@iut-dhaka.edu>
2022-09-01 19:50:35 +0600

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  experimental: {
+    urlImports: [],
+  },
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  // target: "serverless" ,
+  // env: {
+  //   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  //   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  // },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
