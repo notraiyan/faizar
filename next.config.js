8595e497fc260c857ac7dd7ccbc8468766d72b6a
notraiyan <raiyanahmed@iut-dhaka.edu>
2022-09-01 20:04:34 +0600

    add target

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     urlImports: [],
   },
   reactStrictMode: true,
-  // target: "serverless" ,
+  target: "serverless",
   // env: {
   //   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
   //   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
